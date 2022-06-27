0b833aab6a8cd6dc6d6765c6e41a2c0268bd73d5
tungquang99 <tung161099@gmail.com>
2022-06-27 18:28:12 +0700

    update

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -15,6 +15,44 @@ function Map() {
         zoom: 16
   });
 
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(59)
+  const [hourse, setHours] = useState(2)
+
+
+
+  function updateTime() {
+    if (minutes === 0 && seconds === 0) {
+      //reset
+      setSeconds(0);
+      setMinutes(59);
+    }
+    else {
+      if (seconds === 0) {
+        setMinutes(minutes => minutes - 1);
+        setSeconds(59);
+      } else if (minutes === 0) {
+        setHours(hourse => hourse - 1);
+      }
+      else {
+        setSeconds(seconds => seconds - 1);
+      }
+    }
+  }
+
+
+
+  useEffect(() => {
+    // use set timeout and be confident because updateTime will cause rerender
+    // rerender mean re call this effect => then it will be similar to how setinterval works
+    // but with easy to understand logic
+    const token = setTimeout(updateTime, 1000)
+
+    return function cleanUp() {
+      clearTimeout(token);
+    }
+  })
+
   
 
   const changeView = (viewport) => {
@@ -25,7 +63,7 @@ function Map() {
     return (
         <div className='img-map'>
           
-            <div className='text-map'>Vị trí của bạn được chia sẻ còn lại trong: 2:00:00 <Link to='/system' className='btn btn-lock'>Dừng chia sẻ</Link></div>
+            <div className='text-map'>Vị trí của bạn được chia sẻ còn lại trong: {hourse < 10 ? '0' + hourse : hourse}:{minutes < 10 ? '0' + minutes : minutes}:{seconds < 10 ? '0' + seconds : seconds} <Link to='/system' className='btn btn-lock'>Dừng chia sẻ</Link></div>
         </div>
     );
 }
