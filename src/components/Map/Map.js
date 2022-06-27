35e63839b40983280180109dbbf18c19e4cd28ba
tungquang99 <tung161099@gmail.com>
2022-06-27 18:14:32 +0700

    update

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import ReactMapGL, {Marker} from 'react-map-gl';
 import { Link } from 'react-router-dom';
+import { IMAGE } from '../../contants/IMAGE';
 import './map.scss';
 
 function Map() {
@@ -22,21 +23,10 @@ function Map() {
   }
 
     return (
-             <ReactMapGL 
-                {...viewport}
-                 mapStyle="mapbox://styles/mapbox/streets-v11"
-                onViewportChange={(viewport) => changeView(viewport)}
-                mapboxAccessToken="pk.eyJ1IjoidHVuZ3Blbzk5IiwiYSI6ImNsNHdqbHQ2ejFnNDkzZWxhMnM5cWlkYmQifQ.kwMkAvE6Q3DQpGjKMTx1ag">
-                    <Marker
-                        latitude={10}
-                        longitude={90}
-                        offsetLeft={-20}
-                        offsetTop={-30}
-                    >
-                    </Marker>
-                    <div className='text-map'>Vị trí của bạn được chia sẻ còn lại trong: 2:00:00 <Link to='/system' className='btn btn-lock'>Dừng chia sẻ</Link></div>
-                    
-            </ReactMapGL>
+        <div className='img-map'>
+          
+            <div className='text-map'>Vị trí của bạn được chia sẻ còn lại trong: 2:00:00 <Link to='/system' className='btn btn-lock'>Dừng chia sẻ</Link></div>
+        </div>
     );
 }
 
